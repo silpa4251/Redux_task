cac72b8c62da7e100453c63671e3ae6d5ae14849
silpa4251 <silpadas4251@gmail.com>
2024-10-03 19:30:24 +0530

    caps

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -5,7 +5,10 @@ import { useNavigate } from 'react-router-dom';
 const Display = () => {
     const navigate = useNavigate();
     const name = useSelector((state) => state.input.name );
-    const sorted = name.split('').sort().join('');
+    const sorted = typeof name === 'string' ? 
+    name.split('').sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())).join('') :
+    'Invalid input: Not a string';
+  
   return (
     <div>
         <h1>Your String in Sorted Order.....</h1>
